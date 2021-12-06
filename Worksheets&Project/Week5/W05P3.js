b9dd05eec9e927cebf3793f51cc9e05e145843be
claudiabudimir <48676917+claudiabudimir@users.noreply.github.com>
2021-12-06 19:19:22 +0100

    Worksheet5 P3 modified2

--- Worksheets&Project/Week5/W05P3.js
+++ Worksheets&Project/Week5/W05P3.js
@@ -1,4 +1,4 @@
-var obj_fn = 'final_pokeball.obj'
+var obj_fn = 'teapot.obj'
 
 var g3_objDoc = null; // Info parsed from OBJ file
 var g3_drawingInfo = null; // Info for drawing the 3D model with WebGL
