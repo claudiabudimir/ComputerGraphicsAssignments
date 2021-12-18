9aaf03214629c5a97fb207eaed0fac3d75472222
claudiabudimir <claudiabudimir1354@gmail.com>
2021-12-18 18:04:02 +0100

    Worksheet 9

--- worksheet09/Part1.js
+++ worksheet09/Part1.js
@@ -1,7 +1,6 @@
 var obj_fn = 'teapot.obj'
-
-var g1_objDoc = null; // Info parsed from OBJ file
-var g1_drawingInfo = null; // Info for drawing the 3D model with WebGL
+var g1_objDoc = null; 
+var g1_drawingInfo = null;
 
 function initAttributeVariable(gl, attribute, buffer, num, type) {
     gl1.bindBuffer(gl1.ARRAY_BUFFER, buffer);
@@ -9,49 +8,38 @@ function initAttributeVariable(gl, attribute, buffer, num, type) {
     gl1.enableVertexAttribArray(attribute);
 }
 
-// Create a buffer object and perform the initial configuration
-function initobj(gl1, program) {
-
-    var o = new Object();
-
-    o.vertexBuffer = createEmptyArrayBuffer(gl1, program.a_Position, 3, gl1.FLOAT);
-    o.normalBuffer = createEmptyArrayBuffer(gl1, program.a_Normal, 3, gl1.FLOAT);
-    o.colorBuffer = createEmptyArrayBuffer(gl1, program.a_Color, 4, gl1.FLOAT);
-    o.indexBuffer = gl1.createBuffer();
-
-    return o;
+function inititialize_object_buffers(gl1, program) {
+    var obj = new Object();
+    obj.vertexBuffer = createEmptyArrayBuffer(gl1, program.a_Position, 3, gl1.FLOAT);
+    obj.normalBuffer = createEmptyArrayBuffer(gl1, program.a_Normal, 3, gl1.FLOAT);
+    obj.colorBuffer = createEmptyArrayBuffer(gl1, program.a_Color, 4, gl1.FLOAT);
+    obj.indexBuffer = gl1.createBuffer();
+    return obj;
 }
 
-function initground(gl1, program, p, t) {
-    var o = new Object();
-
-    o.vertexBuffer = createEmptyArrayBuffer(gl1, program.position, 3, gl1.FLOAT);
+function initialize_playground_buffers(gl1, program, p, t) {
+    var obj = new Object();
+    obj.vertexBuffer = createEmptyArrayBuffer(gl1, program.position, 3, gl1.FLOAT);
     gl1.bufferData(gl1.ARRAY_BUFFER, flatten(p), gl1.STATIC_DRAW);
-    o.texBuffer = createEmptyArrayBuffer(gl1, program.tex, 2, gl1.FLOAT);
+    obj.texBuffer = createEmptyArrayBuffer(gl1, program.tex, 2, gl1.FLOAT);
     gl1.bufferData(gl1.ARRAY_BUFFER, flatten(t), gl1.STATIC_DRAW);
-
-    return o;
-
+    return obj;
 }
 
 function createEmptyArrayBuffer(gl1, a_attribute, num, type) {
-
-    var buffer = gl1.createBuffer(); // Create a buffer object
-
+    var buffer = gl1.createBuffer(); 
     gl1.bindBuffer(gl1.ARRAY_BUFFER, buffer);
     gl1.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
-    gl1.enableVertexAttribArray(a_attribute); // Enable the assignment
-
+    gl1.enableVertexAttribArray(a_attribute); 
     return buffer;
 }
 
 function onReadComplete(gl1, model, objDoc) {
-    // Acquire the vertex coordinates and colors from OBJ file
+   
     var drawingInfo = objDoc.getDrawingInfo();
 
-    // Write date into the buffer object
     gl1.bindBuffer(gl1.ARRAY_BUFFER, model.vertexBuffer);
-    gl1.bufferData(gl1.ARRAY_BUFFER, drawingInfo.vertices, gl1.STATIC_DRAW);
+    gl1.bufferData(gl1.ARRAY_BUFFER, drawingInfo.vertices_coordinates, gl1.STATIC_DRAW);
 
     gl1.bindBuffer(gl1.ARRAY_BUFFER, model.normalBuffer);
     gl1.bufferData(gl1.ARRAY_BUFFER, drawingInfo.normals, gl1.STATIC_DRAW);
@@ -59,7 +47,6 @@ function onReadComplete(gl1, model, objDoc) {
     gl1.bindBuffer(gl1.ARRAY_BUFFER, model.colorBuffer);
     gl1.bufferData(gl1.ARRAY_BUFFER, drawingInfo.colors, gl1.STATIC_DRAW);
 
-    // Write the indices to the buffer object
     gl1.bindBuffer(gl1.ELEMENT_ARRAY_BUFFER, model.indexBuffer);
     gl1.bufferData(gl1.ELEMENT_ARRAY_BUFFER, drawingInfo.indices, gl1.STATIC_DRAW);
 
@@ -74,13 +61,13 @@ function readOBJFile(fileName, gl1, model, scale, reverse) {
             onReadOBJFile1(request.responseText, fileName, scale, reverse);
         }
     }
-    request.open('GET', fileName, true); // Create a request to get file
-    request.send(); // Send the request
+    request.open('GET', fileName, true); 
+    request.send(); 
 }
 
 function onReadOBJFile1(fileString, fileName, scale, reverse) {
 
-    var objDoc = new OBJDoc(fileName); // Create a OBJDoc object
+    var objDoc = new OBJDoc(fileName);
     var result = objDoc.parse(fileString, scale, reverse);
     if (!result) {
         g1_objDoc = null; g1_drawingInfo = null;
@@ -96,52 +83,38 @@ function init() {
     canvas = document.getElementById("gl-canvas1");
     gl1 = WebGLUtils.setupWebGL(canvas);
 
-    if (!gl1) {
-        alert("WebGL isn�t available");
-    }
-
     gl1.viewport(0, 0, canvas.width, canvas.height);
     gl1.clearColor(0.3921, 0.5843, 0.9294, 1.0);
     gl1.clear(gl1.COLOR_BUFFER_BIT);
     gl1.enable(gl1.DEPTH_TEST);
 
-
-    // Object
-    var program_obj = initShaders(gl1, "vertex-shader-1obj", "fragment-shader-1obj");
+    var program_obj = initShaders(gl1, "vertex-shader-object", "fragment-shader-object");
     gl1.useProgram(program_obj);
-
     var modelobj = initObject();
 
     function initObject() {
-
         program_obj.a_Position = gl1.getAttribLocation(program_obj, 'a_Position');
         program_obj.a_Normal = gl1.getAttribLocation(program_obj, 'a_Normal');
         program_obj.a_Color = gl1.getAttribLocation(program_obj, 'a_Color');
-        // Prepare empty buffer objects for vertex coordinates, colors, and normals
-        var model = initobj(gl1, program_obj);
-
-        // Start reading the OBJ file
-        readOBJFile(obj_fn, gl1, model, 1.0, true); //Scaled
-
+        var model = inititialize_object_buffers(gl1, program_obj);
+        readOBJFile(obj_fn, gl1, model, 1.0, true);
         return model;
     }
 
-    var program_ground1 = initShaders(gl1, "vertex-shader-1ground", "fragment-shader-1ground");
-
-
-    gl1.useProgram(program_ground1);
+    var playground = initShaders(gl1, "vertex-shader-ground", "fragment-shader-ground");
+    gl1.useProgram(playground);
 
     function initGround() {
 
-        program_ground1.position = gl1.getAttribLocation(program_ground1, "a_Position");
-        program_ground1.tex = gl1.getAttribLocation(program_ground1, "a_TexCoord");
+        playground.position = gl1.getAttribLocation(playground, "a_Position");
+        playground.tex = gl1.getAttribLocation(playground, "a_TexCoord");
 
-        var large_rect = [vec3(-2.0, -1.0, -1.0), vec3(-2.0, -1.0, -5.0), vec3(2.0, -1.0, -1.0), vec3(2.0, -1.0, -5.0)]; // 2,0,1   1,3,2
-        var all_points = [large_rect[2], large_rect[0], large_rect[1], large_rect[1], large_rect[3], large_rect[2]];
+        var ground = [vec3(-2.0, -1.0, -1.0), vec3(-2.0, -1.0, -5.0), vec3(2.0, -1.0, -1.0), vec3(2.0, -1.0, -5.0)]; // 2,0,1   1,3,2
+        var vertices_coordinates = [ground[2], ground[0], ground[1], ground[1], ground[3], ground[2]];
         var tex_coord = [vec2(-1.0, 1.0), vec2(-1.0, -1.0), vec2(1.0, 1.0), vec2(1.0, -1.0)]; //change
-        var all_tex = [tex_coord[2], tex_coord[0], tex_coord[1], tex_coord[1], tex_coord[3], tex_coord[2]];
+        var textures_coordinates = [tex_coord[2], tex_coord[0], tex_coord[1], tex_coord[1], tex_coord[3], tex_coord[2]];
 
-        var modelground = initground(gl1, program_ground1, all_points, all_tex);
+        var modelground = initialize_playground_buffers(gl1, playground, vertices_coordinates, textures_coordinates);
 
         return modelground;
     }
@@ -156,7 +129,7 @@ function init() {
         gl1.activeTexture(gl1.TEXTURE0)
         gl1.bindTexture(gl1.TEXTURE_2D, texture);
 
-        gl1.uniform1i(gl1.getUniformLocation(program_ground1, "texMap"), 0);
+        gl1.uniform1i(gl1.getUniformLocation(playground, "texMap"), 0);
 
         gl1.pixelStorei(gl1.UNPACK_FLIP_Y_WEBGL, true);
         gl1.texImage2D(gl1.TEXTURE_2D, 0, gl1.RGB, gl1.RGB, gl1.UNSIGNED_BYTE, image);
@@ -196,9 +169,9 @@ function init() {
 
         gl1.clear(gl1.COLOR_BUFFER_BIT | gl1.DEPTH_BUFFER_BIT);
 
-        gl1.useProgram(program_ground1);
+        gl1.useProgram(playground);
 
-        var pm_ground = gl1.getUniformLocation(program_ground1, 'projectMatrix');
+        var pm_ground = gl1.getUniformLocation(playground, 'projectMatrix');
         var pj_ground = perspective(fovy, aspect, near, far);
         if (above) {
             pj_groundabove = lookAt(vec3(0.0, 1.0, -2.999999), vec3(0.0, 0.0, -3.0), vec3(0.0, 1.0, 0.0));
@@ -207,16 +180,16 @@ function init() {
         gl1.uniformMatrix4fv(pm_ground, false, flatten(pj_ground));
 
 
-        var mv_ground = gl1.getUniformLocation(program_ground1, "modelViewMatrix");
+        var mv_ground = gl1.getUniformLocation(playground, "modelViewMatrix");
         var mvm_ground;
 
         mvm_ground = lookAt(eye, at, up);
         gl1.uniformMatrix4fv(mv_ground, false, flatten(mvm_ground));
 
-        initAttributeVariable(gl1, program_ground1.position, modelground.vertexBuffer, 3, gl1.FLOAT);
-        initAttributeVariable(gl1, program_ground1.tex, modelground.texBuffer, 2, gl1.FLOAT);
+        initAttributeVariable(gl1, playground.position, modelground.vertexBuffer, 3, gl1.FLOAT);
+        initAttributeVariable(gl1, playground.tex, modelground.texBuffer, 2, gl1.FLOAT);
 
-        gl1.uniform1i(gl1.getUniformLocation(program_ground1, "texMap"), 0);
+        gl1.uniform1i(gl1.getUniformLocation(playground, "texMap"), 0);
         gl1.drawArrays(gl1.TRIANGLES, 0, 6);
 
         // BOTH OBJ AND SHADOWS
